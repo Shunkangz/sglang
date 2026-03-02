9aad0a25dbce655265125e9bcb4edda0f782b6fd
Shunkang <182541032+Shunkangz@users.noreply.github.co>
2026-03-02 02:14:47 -0800

    Fix allreduce related issue

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1987,6 +1987,7 @@ class ServerArgs:
             ]
             and (is_sm90_supported() or is_sm100_supported())
             and not self.enable_dp_attention
+            and self.attn_cp_size <= 1
             and self.nnodes == 1
             and not is_h20_device
             and self.moe_a2a_backend == "none"
